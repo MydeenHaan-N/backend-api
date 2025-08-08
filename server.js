69aa207928a6833e37df6abfb456fecf2375f497
Mydeen Haan-N <nmydeenhaan@gmail.com>
2025-08-08 15:15:07 +0530

    Added server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb+srv://2312080:Mydeen2006@clustername.vsl32ko.mongodb.net/?retryWrites=true&w=majority&appName=Clustername', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB Atlas Connected"))
+.catch(err => console.log(err));
+
+const itemRoutes = require('./routes/itemRoutes');
+app.use('/api/items', itemRoutes);
+
+app.listen(5000, () => console.log("Server running on port 5000"));
